d4e5260a31ec0eceb8d2eea50b78b7b2e174ddf1
David Persson <nperson@gmx.de>
2017-11-03 09:05:50 +0100

    Document and format node.go.

--- node.go
+++ node.go
@@ -18,6 +18,7 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+// Node represents a directory inside the design definitions tree.
 type Node struct {
 	path     string
 	Title    string  `json:"title"`
@@ -26,14 +27,17 @@ type Node struct {
 	Children []*Node `json:"children"`
 }
 
+// Meta data as specified in a node configuration file.
 type NodeMeta struct {
-	// optional, if missing will use the URL
+	// Optional, if missing will use the URL.
 	Import string    `json:"import"`
 	Demo   []PropSet `json:"demo"`
 }
 
+// A set of component properties, usually parsed from JSON.
 type PropSet interface{}
 
+// Constructs a new node using its path in the filesystem.
 func NewNodeFromPath(path string, root string) *Node {
 	var url string
 	if path == root {
@@ -48,6 +52,60 @@ func NewNodeFromPath(path string, root string) *Node {
 	}
 }
 
+// Recursively crawls the given root directory, constructing a flat list
+// of nodes.
+func NewNodeListFromPath(root string) ([]*Node, error) {
+	var nodes []*Node
+
+	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if f.IsDir() {
+			nodes = append(nodes, NewNodeFromPath(path, root))
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to build node tree in %s: %s", root, err)
+	}
+
+	for _, n := range nodes {
+		for _, sn := range nodes {
+			if filepath.Dir(sn.path) == n.path {
+				n.Children = append(n.Children, sn)
+			}
+			// TODO: When adding parent, the conversion to JSON will create
+			// recursions.
+			// if filepath.Dir(n.path) == sn.path {
+			// 	n.Parent = sn
+			// }
+		}
+	}
+
+	return nodes, nil
+}
+
+// Reads index.json file when present and returns values. When index.json
+// is not present will simply return an empty Meta.
+func (n Node) Meta() (NodeMeta, error) {
+	var meta NodeMeta
+	f := filepath.Join(n.path, "index.json")
+
+	if _, err := os.Stat(f); os.IsNotExist(err) {
+		return meta, nil
+	}
+
+	content, err := ioutil.ReadFile(f)
+	if err != nil {
+		return meta, err
+	}
+	if err := json.Unmarshal(content, &meta); err != nil {
+		return meta, err
+	}
+	return meta, nil
+}
+
 // Result is passed as component import name to renderComponent()
 // JavaScript glue function.
 func (n Node) Import() (string, error) {
@@ -59,18 +117,6 @@ func (n Node) Import() (string, error) {
 		return m.Import, nil
 	}
 	return n.URL, nil
-}
-
-func (n Node) Crumbs() map[string]string {
-	var crumbs map[string]string // maps url to title
-	//
-	//	parts := strings.Split(n.url, "/")
-	//
-	//	for _, p := range parts {
-	//		title := p
-	//		url :=
-	//	}
-	return crumbs
 }
 
 func (n Node) CSS() (bytes.Buffer, error) {
@@ -104,24 +150,33 @@ func (n Node) bundledAssets(suffix string) (bytes.Buffer, error) {
 	return b, nil
 }
 
-// Reads index.json file when present and returns values. When index.json
-// is not present will simply return an empty Meta.
-func (n Node) Meta() (NodeMeta, error) {
-	var meta NodeMeta
-	f := filepath.Join(n.path, "index.json")
+// Returns documentation parsed from markdown into HTML format.
+func (n Node) Documentation() (template.HTML, error) {
+	file := filepath.Join(n.path, "readme.md")
 
-	if _, err := os.Stat(f); os.IsNotExist(err) {
-		return meta, nil
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return "", nil
 	}
-
-	content, err := ioutil.ReadFile(f)
+	contents, err := ioutil.ReadFile(file)
 	if err != nil {
-		return meta, err
+		return "", err
 	}
-	if err := json.Unmarshal(content, &meta); err != nil {
-		return meta, err
-	}
-	return meta, nil
+	return template.HTML(blackfriday.Run(contents)), nil
+}
+
+// Returns a mapping of URLs to title strings for easily creating a
+// breadcrumb navigation. The last element is the current active one.
+// Does not include the very root element.
+func (n Node) Crumbs() map[string]string {
+	var crumbs map[string]string // maps url to title
+	// TODO
+	//	parts := strings.Split(n.url, "/")
+	//
+	//	for _, p := range parts {
+	//		title := p
+	//		url :=
+	//	}
+	return crumbs
 }
 
 func (n Node) GetDemos() ([]PropSet, error) {
@@ -138,48 +193,4 @@ func (n Node) GetDemo(index int) (PropSet, error) {
 		return nil, err
 	}
 	return meta.Demo[index], nil
-}
-
-// Returns documentation in HTML
-func (n Node) Documentation() (template.HTML, error) {
-	file := filepath.Join(n.path, "readme.md")
-
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return "", nil
-	}
-	contents, err := ioutil.ReadFile(file)
-	if err != nil {
-		return "", err
-	}
-	return template.HTML(blackfriday.Run(contents)), nil
-}
-
-func NewNodeListFromPath(root string) ([]*Node, error) {
-	var nodes []*Node
-
-	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if f.IsDir() {
-			nodes = append(nodes, NewNodeFromPath(path, root))
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to build node tree in %s: %s", root, err)
-	}
-
-	for _, n := range nodes {
-		for _, sn := range nodes {
-			if filepath.Dir(sn.path) == n.path {
-				n.Children = append(n.Children, sn)
-			}
-			//			if filepath.Dir(n.path) == sn.path {
-			//				n.Parent = sn
-			//			}
-		}
-	}
-
-	return nodes, nil
 }
