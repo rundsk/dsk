c030728e93b228914ce20a75d0782f8388c189ae
Marius Wilms <mariuswilms@gmx.de>
2018-03-15 13:35:49 +0100

    Refactor doc post processing

--- node_doc.go
+++ node_doc.go
@@ -41,9 +41,18 @@ func (d NodeDoc) Title() string {
 
 // HTML as parsed from the underlying file.
 func (d NodeDoc) HTML() ([]byte, error) {
+	contents, err := ioutil.ReadFile(d.path)
+	if err != nil {
+		return nil, err
+	}
+
 	switch filepath.Ext(d.path) {
 	case ".md", ".markdown":
-		return d.parseMarkdown()
+		parsed, err := d.parseMarkdown(contents)
+		if err != nil {
+			return parsed, err
+		}
+		return d.postprocessHTML(parsed)
 	}
 	return nil, fmt.Errorf("document %s is not in a supported format", d.path)
 }
@@ -59,7 +68,7 @@ func (d NodeDoc) Raw() ([]byte, error) {
 
 // Parses markdown into HTML and makes relative links absolute, so
 // they are more portable.
-func (d NodeDoc) parseMarkdown() ([]byte, error) {
+func (d NodeDoc) parseMarkdown(contents []byte) ([]byte, error) {
 	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
 		Flags: blackfriday.CommonHTMLFlags &^ blackfriday.UseXHTML,
 	})
@@ -67,82 +76,83 @@ func (d NodeDoc) parseMarkdown() ([]byte, error) {
 		blackfriday.Strikethrough | blackfriday.NoEmptyLineBeforeBlock&^
 		blackfriday.HeadingIDs&^blackfriday.DefinitionLists
 
-	contents, err := ioutil.ReadFile(d.path)
-	if err != nil {
-		return nil, err
-	}
-	parsed := blackfriday.Run(
+	return blackfriday.Run(
 		contents,
 		blackfriday.WithRenderer(renderer),
 		blackfriday.WithExtensions(extensions),
-	)
+	), nil
+}
 
-	// Works around buggy AbsolutePrefix feature in blackfriday. We
-	// need to have all URLs absolute as documents can be placed
-	// anywhere inside the frontend's URL structure. The workaround
-	// can possibly be removed once PR #231 or a change functionally
-	// equal to it has been implemented.
-	//
-	// https://github.com/russross/blackfriday/pull/231
-	// https://github.com/russross/blackfriday/commit/27ba4cebef7f37e0bb5685e23cb7213cd809f9e8
-	// https://github.com/russross/blackfriday/commit/5c12499aa1ddda74561fb899c394f01fd1e8e9e6
-	makeURLsAbsolute := func(parsed []byte, prefix string) ([]byte, error) {
-		var buf bytes.Buffer
+// Post-processes given HTML after it has been processed by i.e. the
+// file-type specific parser.
+//
+// - Makes the HTML more portable, by turning relative source links
+//   into absolute ones.
+//
+//   Works around buggy AbsolutePrefix feature in blackfriday. We
+//   need to have all source URLs absolute as documents can be placed
+//   anywhere inside the frontend's URL structure. The workaround can
+//   possibly be removed once PR #231 or a change functionally equal
+//   to it has been implemented.
+//
+//   https://github.com/russross/blackfriday/pull/231
+//   https://github.com/russross/blackfriday/commit/27ba4cebef7f37e0bb5685e23cb7213cd809f9e8
+//   https://github.com/russross/blackfriday/commit/5c12499aa1ddda74561fb899c394f01fd1e8e9e6
+func (d NodeDoc) postprocessHTML(contents []byte) ([]byte, error) {
+	var buf bytes.Buffer
 
-		// Ensure last path element isn't recognized as a file.
-		uBase, err := url.Parse(prefix + "/")
-		if err != nil {
-			return buf.Bytes(), err
-		}
-
-		z := html.NewTokenizer(bytes.NewReader(parsed))
-
-		// Helper to get an attribute value from a token.
-		attr := func(t html.Token, name string) (bool, int, string) {
-			for key, a := range t.Attr {
-				if a.Key == name {
-					return true, key, a.Val
-				}
-			}
-			return false, 0, ""
-		}
-
-		for {
-			tt := z.Next()
-			t := z.Token()
-
-			switch tt {
-			case html.ErrorToken:
-				err := z.Err()
-
-				if err == io.EOF {
-					return buf.Bytes(), nil
-				}
-				return buf.Bytes(), err
-			case html.StartTagToken, html.SelfClosingTagToken:
-				var ok bool
-				var key int
-				var v string
-
-				switch t.Data {
-				case "img", "video":
-					ok, key, v = attr(t, "src")
-				}
-				if !ok {
-					continue
-				}
-				u, err := url.Parse(v)
-				if err != nil {
-					return buf.Bytes(), err
-				}
-				if u.IsAbs() {
-					continue
-				}
-				t.Attr[key].Val = uBase.ResolveReference(u).String()
-			}
-			buf.WriteString(t.String())
-		}
+	// Ensure last path element isn't recognized as a file.
+	uBase, err := url.Parse(d.URLPrefix + "/")
+	if err != nil {
+		return buf.Bytes(), err
 	}
 
-	return makeURLsAbsolute(parsed, d.URLPrefix)
+	z := html.NewTokenizer(bytes.NewReader(contents))
+
+	// Helper to get an attribute value from a token.
+	attr := func(t html.Token, name string) (bool, int, string) {
+		for key, a := range t.Attr {
+			if a.Key == name {
+				return true, key, a.Val
+			}
+		}
+		return false, 0, ""
+	}
+
+	for {
+		tt := z.Next()
+		t := z.Token()
+
+		switch tt {
+		case html.ErrorToken:
+			err := z.Err()
+
+			if err == io.EOF {
+				return buf.Bytes(), nil
+			}
+			return buf.Bytes(), err
+		case html.StartTagToken, html.SelfClosingTagToken:
+			var ok bool
+			var key int
+			var v string
+
+			switch t.Data {
+			case "img", "video":
+				ok, key, v = attr(t, "src")
+			}
+			if !ok {
+				continue
+			}
+			u, err := url.Parse(v)
+			if err != nil {
+				return buf.Bytes(), err
+			}
+			if u.IsAbs() {
+				continue
+			}
+			t.Attr[key].Val = uBase.ResolveReference(u).String()
+		}
+		buf.WriteString(t.String())
+	}
+	return buf.Bytes(), nil
 }
