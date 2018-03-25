3c1b69cd9cd9a7ae19a934fa7fb934a2f8b1ea31
Marius Wilms <mariuswilms@gmx.de>
2018-03-25 19:19:07 +0200

    Bring back builtin frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,197 +6,281 @@
  */
 
 /* globals Tree: false */
+/* globals Client: false */
+/* globals Changes: false */
 /* globals URLSearchParams: false */
-
-"use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
   const $ = document.querySelectorAll.bind(document);
   const $1 = document.querySelector.bind(document);
 
-  let header = $1('header');
+  const baseTitle = document.title;
+
+  let tree = new Tree();
+  let changes = Client.changes();
+
+  let activeNode = null;
+  let activeTree = null;
+  let activeSearch = null;
+
   let main = $1('main');
   let nav = $1('.tree-nav');
   let searchField = $1('.search__field');
   let searchClear = $1('.search__clear');
 
-  let tree = new Tree();
-  let baseTitle = document.title;
+  //
+  // Changes
+  //
+
+  changes.onmessage = (ev) => {
+    // TODO: Is the current node displayed still present?
+
+    tree.sync().then(() => {
+      renderNav(tree.root, activeNode);
+    });
+
+    console.log(ev);
+  };
+
+  changes.onclose = (ev) => {
+    console.log('Disconnected');
+  };
+
+  changes.onerror = (err) => {
+    console.log('Error:', err);
+  };
+
+
+  //
+  // Global State
+  //
 
   // Initializes the tree, the left hand tree navigation and displays
   // the currently route selected node - if any.
-  tree.sync()
-    .then(() => {
-      return tree.search();
-    })
-    .then((results) => {
-      // navigateToNode(window.location.pathname, true);
-      // handleSearchWithQuery(window.location.search.substring(1));
+  (function initialState() {
+    let params = new URLSearchParams(window.location.search);
+    let get = Client.get(window.location.pathname.replace('/tree/', ''));
+    let sync = tree.sync();
+
+    Promise.all([get, sync]).then((vals) => {
+      restoreState(params.get('q'), vals[0]);
     });
+  }());
 
 
-  // main.querySelectorAll('.text a, .crumbs a, .keywords a, .children-table a').forEach((el) => {
-  main.addEvenListener('click', (ev) => {
-    if (ev.target && ev.target.nodeName == 'A') {
-      console.log('Clicked link in main!', ev.target);
-      /// el.addEventListener('click', handleTextLinkClick);
+  // Restore previous state, when navigating back and forth.
+  window.addEventListener('popstate', (ev) => {
+    if (ev.state) {
+      restoreState(ev.state.search, ev.state.node);
     }
   });
-  header.querySelector('a').addEventListener('click', handleTextLinkClick);
+
+  function restoreState(q, n) {
+    searchField.value = q;
+
+    setTitle(n);
+    renderNode(n);
+    // Must wait until we have nav, to activateNode.
+    activeNode = n;
+
+    if (!q) {
+      renderNav(tree.root, activeNode);
+    } else {
+      Client.search(q).then((results) => {
+        renderNav(tree.filteredBy(results).root, activeNode);
+      });
+    }
+  }
+
+  function setTitle(n) {
+    document.title = (n ? `${baseTitle}: ${n.title}` : baseTitle);
+  }
+
+  //
+  // Node Display
+  //
+
+  // main.querySelectorAll('.text a, .crumbs a, .keywords a, .children-table a').forEach((el) => {
+  main.addEventListener('click', (ev) => {
+    if (ev.metaKey || !ev.target || ev.target.nodeName !== 'A') {
+      return;
+    }
+    ev.preventDefault();
+
+    // Tag-links are not links to be followed, but queries
+    // to be entered into the search bar.
+
+    // Differntiate between node links and other links.
+    console.log('Clicked link in main!', ev.target);
+  });
 
   // Load the node based on node URL path.
-  function navigateToNode(url, initialNavigation) {
-    return tree.get(url).then((node) => {
+  function navigateToNode(nurl, replaceState) {
+    return Client.get(nurl).then((n) => {
       let params = new URLSearchParams(window.location.search);
+      let q = params.get('q');
 
-      history[initialNavigation ? 'replaceState' : 'pushState'](
-          {node: node, search: params.get('q')},
-          '' ,
-          '/' + url + '?q=' + params.get('q')
-        );
+      history[replaceState ? 'replaceState' : 'pushState'](
+        { node: n, search: q },
+        '',
+        q ? `/tree/${nurl}?q=${q}` : `/tree/${nurl}`,
+      );
 
-      document.title = baseTitle + ': ' + node.title;
-      main.innerHTML = renderNode(node);
-      activateNode(node);
+      setTitle(n);
+      renderNode(n);
+      activeNode = n;
+
+      let active;
+      active = nav.querySelector('.is-active');
+      if (active) {
+        active.classList.remove('is-active');
+      }
+      active = nav.querySelector(`a[href="/tree/${n.url}"]`);
+      if (active) {
+        active.parentNode.classList.add('is-active');
+      }
     });
   }
 
-  //
-  // HTML Rendering
-  //
+  function renderNode(n) {
+    main.innerHTML = '';
 
-  // TODO Implement.
-  function renderNode(node) {
-    return node.description;
+    let title = document.createElement('section');
+    title.classList.add('title');
+    title.innerHTML = `<h1>${n.title}</h1>`;
+
+    main.appendChild(title);
+
+    if (n.description) {
+      let description = document.createElement('section');
+      description.classList.add('description');
+      description.innerHTML = n.description;
+
+      main.appendChild(description);
+    }
+
+    if (n.tags.length) {
+      let tags = document.createElement('section');
+      tags.classList.add('tags');
+
+      let ul = document.createElement('ul');
+
+      n.tags.forEach((tag) => {
+        let li = document.createElement('li');
+        li.classList.add('tags__tag');
+
+        let a = document.createElement('a');
+        a.href = `/tree/${n.url}?q=${tag}`;
+        a.innerText = tag;
+
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+
+      tags.appendChild(ul);
+      main.appendChild(tags);
+    }
+
+    let info = document.createElement('div');
+    info.classList.add('info');
+
+    main.appendChild(info);
+
+    let twoColumns = document.createElement('div');
+    twoColumns.classList.add('two-columns');
+
+    info.appendChild(twoColumns);
+
+    if (n.docs[0]) {
+      let text = document.createElement('div');
+      text.classList.add('text');
+      text.innerHTML = n.docs[0].html;
+
+      twoColumns.appendChild(text);
+    }
   }
 
-  // Renders the nav structure.
-  // FIXME: Append list withouth root node?
-  // if list nav.appendChild(list.childNodes[1]);
-  function renderNav(tree) {
-    return renderList(tree.root);
+  //
+  // Tree Navigation
+  //
+
+  function renderNav(root, activeNode) {
+    nav.innerHTML = '';
+
+    if (root.children.length) {
+      nav.appendChild(renderList(root, activeNode).childNodes[1]);
+    }
   }
 
   // Recursively turns the given tree data into a "ul li" structure.
-  function renderList(node) {
-    let ul = document.createElement('ul');
+  function renderList(n, activeNode) {
     let li = document.createElement('li');
-    let a  = document.createElement('a');
+    let ul = document.createElement('ul');
+    let a = document.createElement('a');
 
-    a.href = "/" + node.url;
-    a.innerHTML = node.title;
-    a.addEventListener('click', handleNav);
-
-    if (node.isGhost) {
-      a.classList.add('ghosted');
+    if (activeNode && n.url === activeNode.url) {
+      li.classList.add('is-active');
     }
-    li.appendChild(a);
-    li.appendChild(ul);
 
-    for (let child in node.children) {
-      let childList = renderList(node.children[child]);
-      if (childList) {
-        ul.appendChild(childList);
+    a.href = `/tree/${n.url}`;
+    a.innerHTML = n.title;
+    a.addEventListener('click', (ev) => {
+      if (ev.metaKey) {
+        return;
       }
+      ev.preventDefault();
+      navigateToNode(n.url);
+    });
+
+    li.appendChild(a);
+
+    if (n.children.length) {
+      li.appendChild(ul);
     }
+    n.children.forEach((c) => {
+      let cl = renderList(c, activeNode);
+      if (cl) {
+        ul.appendChild(cl);
+      }
+    });
     return li;
   }
 
-  function activateNode(node) {
-    let bullets = nav.querySelectorAll('li');
-
-    for (let a of bullets) {
-      a.classList.remove('is-active');
-    }
-
-    let activeNode = bullets.querySelector('a[href="' + decodeURIComponent(node.url) + '"]');
-    if (activeNode) {
-      activeNode.parentNode.classList.add('is-active');
-    }
-  }
-
   //
-  // State Management
+  // Search
   //
-
-  // Runs the search with a given query
-  function handleSearchWithQuery(q) {
-    searchField.value = q;
-
-    // Every search action can be navigated to, just the "q" part changes.
-    history.replaceState(
-      {node: history.current.node, search: q},
-      '',
-      window.location.pathname + '?q=' + q
-    );
-
-    tree.search(q).then((results) => {
-      return tree.filteredBy(results).root;
-    }).then((root) => {
-      nav.innerHTML = renderNav(root, window.location.pathname);
-    });
-  }
 
   searchField.addEventListener('input', function() {
-    handleSearchWithQuery(this.value);
-  });
-  searchClear.addEventListener('click', function() {
-    handleSearchWithQuery("");
+    let q = this.value;
+
+    history.replaceState(
+      { node: activeNode, search: q },
+      '',
+      // Every search action can be navigated to, just the "q" part changes.
+      q ? `${window.location.pathname}?q=${q}` : window.location.pathname,
+    );
+
+    if (!q) {
+      renderNav(tree.root, activeNode);
+    } else {
+      Client.search(q).then((results) => {
+        renderNav(tree.filteredBy(results).root, activeNode);
+      });
+    }
   });
 
-  // Loads the node when a link the nav is clicked
-  // and updates session history (url)
-  let handleNav = function(ev) {
-    // If CMD is pressed default behavior is triggered (open link in new tab)
-    if (event.metaKey) {
+  searchClear.addEventListener('click', () => {
+    searchField.value = '';
+    history.replaceState({ node: activeNode, search: '' }, '', window.location.pathname);
+    renderNav(tree.root, activeNode);
+  });
+
+  // Focus the search field when pressing CMD + k.
+  window.addEventListener('keydown', (ev) => {
+    if (ev.key !== 'k' || !ev.metaKey) {
       return;
     }
-
     ev.preventDefault();
-    loadNodeWithPath(this.pathname, true);
-  };
-
-  // Restore previouse state.
-  window.onpopstate = function(ev) {
-    if (ev.state) {
-      loadNodeWithPath(event.state.path, false);
-      handleSearchWithQuery(event.state.search);
-    }
-  };
-
-  // Foucs the search field when pressing CMD + k.
-  window.addEventListener('keydown', function(ev) {
-    if (ev.key === 'k' && ev.metaKey) {
-      ev.preventDefault();
-      searchField.focus();
-    }
+    searchField.focus();
   });
-
-
-  // Calls the search when a link in text is clicked
-  function handleTextLinkClick(ev) {
-    // When the link starts with "search:" it is not a link to be followed, but
-    // a query to be entered into the search bar.
-    if (this.href.split(":")[0] === "search") {
-      ev.preventDefault();
-      handleSearchWithQuery(this.href.substring(7));
-      return;
-    }
-
-    // If CMD is pressed default behavior is triggered (open link in new tab)
-    if (event.metaKey) {
-      return;
-    }
-
-    // Only handle local links
-    if (this.host === window.location.host) {
-      // Only handle the link, when it is not a file (== the last part of the path doesn’t contain a ".")
-      if (this.href.split("/").pop().split(".").length === 1) {
-        ev.preventDefault();
-        loadNodeWithPath(this.pathname, true);
-      }
-    }
-  }
-
-
 });
