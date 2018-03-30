51c3185b192ca14c5f6d9e71bdf9aa0ee38bcb9d
Marius Wilms <mariuswilms@gmx.de>
2018-03-30 12:36:45 +0200

    Rename util to path, and move all path funcs there

--- node.go
+++ node.go
@@ -14,7 +14,6 @@ import (
 	"path/filepath"
 	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -400,29 +399,4 @@ func lookupNodeURL(url string) string {
 		strings.ToLower(normalizeNodeURL(url)),
 		"",
 	)
-}
-
-// Finds an order number embedded into given path/URL segment and
-// returns it. If none is found, returns 0.
-func orderNumber(segment string) uint64 {
-	s := NodePathTitleRegexp.FindStringSubmatch(segment)
-
-	if len(s) > 2 {
-		parsed, _ := strconv.ParseUint(s[0], 10, 64)
-		return parsed
-	}
-	return 0
-}
-
-// Removes order numbers from path/URL segment, if present.
-func removeOrderNumber(segment string) string {
-	s := NodePathTitleRegexp.FindStringSubmatch(segment)
-
-	if len(s) == 0 {
-		return segment
-	}
-	if len(s) > 2 {
-		return s[2]
-	}
-	return s[1]
 }
--- path.go
+++ path.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -82,4 +83,29 @@ func anyPathSegmentMatches(path string, r *regexp.Regexp) bool {
 		path = filepath.Dir(path)
 	}
 	return false
+}
+
+// Finds an order number embedded into given path segment and
+// returns it. If none is found, returns 0.
+func orderNumber(segment string) uint64 {
+	s := NodePathTitleRegexp.FindStringSubmatch(segment)
+
+	if len(s) > 2 {
+		parsed, _ := strconv.ParseUint(s[0], 10, 64)
+		return parsed
+	}
+	return 0
+}
+
+// Removes order numbers from path segment, if present.
+func removeOrderNumber(segment string) string {
+	s := NodePathTitleRegexp.FindStringSubmatch(segment)
+
+	if len(s) == 0 {
+		return segment
+	}
+	if len(s) > 2 {
+		return s[2]
+	}
+	return s[1]
 }
