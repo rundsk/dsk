f4c1183e6b1ea7315e33cadf10d93d4ff09052ca
Marius Wilms <mariuswilms@mailbox.org>
2019-06-26 16:33:08 +0200

    Add TODO to escape inside CodeBlock

--- frontend/src/CodeBlock/index.js
+++ frontend/src/CodeBlock/index.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './CodeBlock.css';
 import { copyTextToClipboard } from '../utils';
 
+// TODO: HTML-escape inner content, probably using
+// https://stackoverflow.com/questions/6234773/can-i-escape-html-special-chars-in-javascript
+// https://www.npmjs.com/package/escape-html
 function CodeBlock(props) {
   const [copyText, setCopyText] = useState("Copy");
   function copyCode() {
