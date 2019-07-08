555a081724b26b0b67817912edd7f6f72273e379
Christoph Labacher <christoph.labacher@gmail.com>
2019-07-08 12:46:29 +0200

    Add Copied Indicator to ColorCard

--- frontend/src/ColorCard/index.js
+++ frontend/src/ColorCard/index.js
@@ -4,6 +4,8 @@ import { copyTextToClipboard } from '../utils';
 import contrast from 'get-contrast';
 
 function ColorCard(props) {
+  const [showCopiedIndicator, setShowCopiedIndicator] = useState(false);
+
   let classes = ["color-card"];
 
   if (contrast.ratio(props.color, "white") < 1.5) {
@@ -14,11 +16,27 @@ function ColorCard(props) {
     classes.push("color-card--is-compact");
   }
 
-  return (<div className={classes.join(" ")} key={props.id} onClick={() => { copyTextToClipboard(props.color) }}>
+  function copyCode() {
+    setShowCopiedIndicator(true);
+    copyTextToClipboard(props.color);
+
+    setTimeout(() => {
+      setShowCopiedIndicator(false);
+    }, 1000);
+  }
+
+  return (<div className={classes.join(" ")} key={props.id} onClick={copyCode}>
     <div className="color-card__demo" style={{ backgroundColor: props.color }}>
       <div className="color-card__score">
         <span>{contrast.score(props.color, "white")}</span>
         <span>{contrast.score(props.color, "black")}</span>
+      </div>
+
+      <div
+        className={`color-card__copied-indicator ${showCopiedIndicator ? "color-card__copied-indicator--is-visible" : ""}`}
+        style={{ color: contrast.ratio(props.color, "white") < contrast.ratio(props.color, "black") ? "black" : "white"}}
+      >
+        Copied!
       </div>
     </div>
     <div className="color-card__name">{props.children} <span className="color-card__id">({props.id})</span></div>
