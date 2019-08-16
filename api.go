rundsk/dsk

commit 435ff5d26ed3a937e6a3c2db1332e1919b0dda66

Fix HTTP handlers to return 404 on non routed URLs

main.go
@@ -204,12 +204,15 @@ func main() {
 		api.MountHTTPHandlers()
 	}
 
-	// Handles frontend root document delivery and frontend assets.
+	// Handles frontend index document delivery, frontend assets and catch
+	// everything else funnelling it into the general error handler.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if filepath.Ext(r.URL.Path) != "" {
+		if r.URL.Path == "/" || r.URL.Path == "index.html" {
+			frontendIndexHandler(w, r)
+		} else if filepath.Ext(r.URL.Path) != "" {
 			frontendAssetHandler(w, r)
 		} else {
-			frontendRootHandler(w, r)
+			errorHandler(w, r)
 		}
 	})
 
@@ -229,12 +232,7 @@ func main() {
 }
 
 // Serves the frontend's index.html.
-//
-// Handles these kinds of URLs:
-//   /
-//   /index.html
-//   /* <catch all>
-func frontendRootHandler(w http.ResponseWriter, r *http.Request) {
+func frontendIndexHandler(w http.ResponseWriter, r *http.Request) {
 	wr := &HTTPResponder{w, r, ""}
 	path := "index.html"
 
@@ -284,4 +282,9 @@ func frontendAssetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeContent(w, r, info.Name(), info.ModTime(), asset)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	wr := &HTTPResponder{w, r, ""}
+	wr.Error(HTTPErrNotFound, nil)
 }

http.go
@@ -15,6 +15,7 @@ import (
 var (
 	HTTPErr            = &HTTPError{http.StatusInternalServerError, "Techniker ist informiert"}
 	HTTPErrUnsafePath  = &HTTPError{http.StatusBadRequest, "Directory traversal attempt detected!"}
+	HTTPErrNotFound    = &HTTPError{http.StatusNotFound, "Not found"}
 	HTTPErrNoSuchNode  = &HTTPError{http.StatusNotFound, "No such node"}
 	HTTPErrNoSuchAsset = &HTTPError{http.StatusNotFound, "No such asset"}
 )

commit 3f714213bfe78a8c5704b376c0dc4dc0a72639b2

Fix regression introduced in 435ff5d

api.go
@@ -6,5 +6,6 @@
 package main
 
 type API interface {
+	// Handlers must be mounted below /api/v{major version}.
 	MountHTTPHandlers()
 }

apiv1.go
@@ -141,6 +141,7 @@ func (api APIv1) MountHTTPHandlers() {
 	})
 	http.HandleFunc("/api/v1/search", api.SearchHandler)
 	http.HandleFunc("/api/v1/messages", api.MessagesHandler)
+	http.HandleFunc("/api/v1", api.NotFoundHandler)
 }
 
 func (api APIv1) NewHello() *APIv1Hello {
@@ -513,4 +514,9 @@ func (api APIv1) SearchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	wr.OK(api.NewNodeTreeSearchResults(results, total, took))
+}
+
+func (api APIv1) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	wr := &HTTPResponder{w, r, ""}
+	wr.Error(HTTPErrNotFound, nil)
 }

apiv2.go
@@ -62,6 +62,7 @@ func (api APIv2) MountHTTPHandlers() {
 	http.HandleFunc("/api/v2/filter", api.FilterHandler)
 	http.HandleFunc("/api/v2/search", api.SearchHandler)
 	http.HandleFunc("/api/v2/messages", api.v1.MessagesHandler)
+	http.HandleFunc("/api/v2", api.v1.NotFoundHandler)
 }
 
 func (api APIv2) NewNodeTreeSearchResults(hs []*FullSearchHit, total int, took time.Duration) *APIv2FullSearchResults {

main.go
@@ -204,15 +204,14 @@ func main() {
 		api.MountHTTPHandlers()
 	}
 
-	// Handles frontend index document delivery, frontend assets and catch
-	// everything else funnelling it into the general error handler.
+	// Handles frontend root document delivery and frontend assets.
+	// The frontend is allowed to use any path except /api. We route
+	// everything else into the front controller (index.html).
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" || r.URL.Path == "index.html" {
-			frontendIndexHandler(w, r)
-		} else if filepath.Ext(r.URL.Path) != "" {
+		if filepath.Ext(r.URL.Path) != "" {
 			frontendAssetHandler(w, r)
 		} else {
-			errorHandler(w, r)
+			frontendRootHandler(w, r)
 		}
 	})
 
@@ -232,7 +231,12 @@ func main() {
 }
 
 // Serves the frontend's index.html.
-func frontendIndexHandler(w http.ResponseWriter, r *http.Request) {
+//
+// Handles these kinds of URLs:
+//   /
+//   /index.html
+//   /* <catch all>
+func frontendRootHandler(w http.ResponseWriter, r *http.Request) {
 	wr := &HTTPResponder{w, r, ""}
 	path := "index.html"
 
@@ -282,9 +286,4 @@ func frontendAssetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeContent(w, r, info.Name(), info.ModTime(), asset)
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request) {
-	wr := &HTTPResponder{w, r, ""}
-	wr.Error(HTTPErrNotFound, nil)
 }