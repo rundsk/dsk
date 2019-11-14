532ddea51ce28355642d7a19ccf5d119a1246047
Marius Wilms <mariuswilms@mailbox.org>
2019-11-14 09:28:31 +0100

    Fix links to node assets

--- frontend/src/Link/index.js
+++ frontend/src/Link/index.js
@@ -9,8 +9,11 @@ import { BaseLink, withRoute } from 'react-router5';
 // Replace links to internal node with links from the router.
 function Link(props) {
   let node = props['data-node'];
+  let nodeAsset = props['data-node-asset'];
 
-  if (!node) {
+  // Do not use router for non nodes (i.e. external links)
+  // or assets (these should be downloaded directly).
+  if (!node || nodeAsset) {
     return <a href={props.href}>{props.children}</a>;
   }
   // Create an URL Instance from the href String and create an URLSearchParams Instance from it.
