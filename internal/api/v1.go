rundsk/dsk

commit 7f4fa5b8f4553e670b19e2d1c149709496793e0c

Improve CORS support
Use CORS HTTP handler library to do the heavy lifting.

- Add other CORS headers as necessary
- Allow credentials, we might be behind basic auth
- Support preflight requests
- Default to CORS being disabled

internal/api/v1.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 	"github.com/rundsk/dsk/internal/bus"
 	"github.com/rundsk/dsk/internal/config"
 	"github.com/rundsk/dsk/internal/ddt"
@@ -22,12 +23,12 @@ import (
 	"github.com/rundsk/dsk/internal/plex"
 )
 
-func NewV1(ss *plex.Sources, appVersion string, b *bus.Broker, allowOrigin string) *V1 {
+func NewV1(ss *plex.Sources, appVersion string, b *bus.Broker, allowOrigins []string) *V1 {
 	return &V1{
-		appVersion:  appVersion,
-		allowOrigin: allowOrigin,
-		broker:      b,
-		sources:     ss,
+		appVersion:   appVersion,
+		allowOrigins: allowOrigins,
+		broker:       b,
+		sources:      ss,
 		upgrader: websocket.Upgrader{
 			ReadBufferSize:  1024,
 			WriteBufferSize: 1024,
@@ -40,10 +41,13 @@ type V1 struct {
 
 	appVersion string
 
-	// The value of the Access-Control-Allow-Origin HTTP header to set, if empty
-	// the header will remain unset. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
-	// for valid values.
-	allowOrigin string
+	// allowOrigins is a list of origins a cross-domain request can be
+	// executed from. If the special * value is present in the list, all
+	// origins will be allowed. An origin may contain a wildcard (*) to
+	// replace 0 or more characters (i.e.: http://*.domain.com). Usage of
+	// wildcards implies a small performance penality. Only one wildcard
+	// can be used per origin. The default value is *.
+	allowOrigins []string
 
 	// We subscribe to the broker in our messages endpoint.
 	broker *bus.Broker
@@ -158,23 +162,36 @@ type V1Source struct {
 	IsReady bool   `json:"is_ready"`
 }
 
-func (api V1) MountHTTPHandlers() {
-	log.Print("Mounting APIv1 HTTP handlers...")
+// HTTPMux returns a HTTP mux that can be mounted onto a root mux.
+func (api V1) HTTPMux() http.Handler {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/api/v1/hello", api.HelloHandler)
-	http.HandleFunc("/api/v1/config", api.ConfigHandler)
-	http.HandleFunc("/api/v1/sources", api.SourcesHandler)
-	http.HandleFunc("/api/v1/tree", api.TreeHandler)
-	http.HandleFunc("/api/v1/tree/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/hello", api.HelloHandler)
+	mux.HandleFunc("/config", api.ConfigHandler)
+	mux.HandleFunc("/sources", api.SourcesHandler)
+	mux.HandleFunc("/tree", api.TreeHandler)
+	mux.HandleFunc("/tree/", func(w http.ResponseWriter, r *http.Request) {
 		if filepath.Ext(r.URL.Path) != "" {
 			api.NodeAssetHandler(w, r)
 		} else {
 			api.NodeHandler(w, r)
 		}
 	})
-	http.HandleFunc("/api/v1/search", api.SearchHandler)
-	http.HandleFunc("/api/v1/messages", api.MessagesHandler)
-	http.HandleFunc("/api/v1", api.NotFoundHandler)
+	mux.HandleFunc("/search", api.SearchHandler)
+	mux.HandleFunc("/messages", api.MessagesHandler)
+	mux.HandleFunc("/", api.NotFoundHandler)
+
+	// An empty slice of origins indicates that CORS shoule be
+	// disabled. If we'd pass an empty slice to the CORS middleware
+	// it'd be interpreted to allow all origins. We want to be "secure
+	// by default".
+	if len(api.allowOrigins) == 0 {
+		return mux
+	}
+	return cors.New(cors.Options{
+		AllowedOrigins:   api.allowOrigins,
+		AllowCredentials: true,
+	}).Handler(mux)
 }
 
 func (api V1) NewHello(s *plex.Source) (*V1Hello, error) {
@@ -228,7 +245,7 @@ func (api V1) NewNode(n *ddt.Node, s *plex.Source) (*V1Node, error) {
 		return nil, err
 	}
 	for _, v := range nDocs {
-		html, err := v.HTML("/api/v1/tree", n.URL(), s.Tree.Get, s.Name)
+		html, err := v.HTML("/tree", n.URL(), s.Tree.Get, s.Name)
 		if err != nil {
 			return nil, err
 		}
@@ -421,7 +438,7 @@ func (api V1) NewSources(ss *plex.Sources) (*V1Sources, error) {
 // Handles these URLs:
 //   /api/v1/hello
 func (api V1) HelloHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	_, s, err := api.sources.Get("live")
@@ -443,7 +460,7 @@ func (api V1) HelloHandler(w http.ResponseWriter, r *http.Request) {
 // Handles these URLs:
 //   /api/v1/config
 func (api V1) ConfigHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	_, s, err := api.sources.Get("live")
@@ -465,7 +482,7 @@ func (api V1) ConfigHandler(w http.ResponseWriter, r *http.Request) {
 // Handles these URLs:
 //   /api/v1/messages
 func (api *V1) MessagesHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "")
 	defer r.Body.Close()
 
 	conn, err := api.upgrader.Upgrade(w, r, nil)
@@ -513,7 +530,7 @@ func (api *V1) MessagesHandler(w http.ResponseWriter, r *http.Request) {
 //   /api/v1/tree
 //   /api/v1/tree&v={version}
 func (api V1) TreeHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 	// Not getting or checking path, as only tree requests are routed here.
 
@@ -543,10 +560,10 @@ func (api V1) TreeHandler(w http.ResponseWriter, r *http.Request) {
 // Handles these kinds of URLs:
 //   /api/v1/tree/DisplayData/Table?v={version}
 func (api V1) NodeHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
-	path := r.URL.Path[len("/api/v1/tree/"):]
+	path := r.URL.Path[len("/tree/"):]
 	v := r.URL.Query().Get("v")
 
 	s, err := api.sources.MustGet(v)
@@ -590,10 +607,10 @@ func (api V1) NodeHandler(w http.ResponseWriter, r *http.Request) {
 //   /api/v1/tree/Button/colors.json&v={version}
 //   /api/v1/tree/Button/colors.yaml&v={version}
 func (api V1) NodeAssetHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
-	path := r.URL.Path[len("/api/v1/tree/"):]
+	path := r.URL.Path[len("/tree/"):]
 	v := r.URL.Query().Get("v")
 
 	s, err := api.sources.MustGet(v)
@@ -676,7 +693,7 @@ func (api V1) NodeAssetHandler(w http.ResponseWriter, r *http.Request) {
 //   /api/v1/search?q={query}
 //   /api/v1/search?q={query}&v={version}
 func (api V1) SearchHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	q := r.URL.Query().Get("q")
@@ -702,7 +719,7 @@ func (api V1) SearchHandler(w http.ResponseWriter, r *http.Request) {
 // Handles this URL:
 //   /api/v1/sources
 func (api V1) SourcesHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	ss, err := api.NewSources(api.sources)
@@ -714,7 +731,7 @@ func (api V1) SourcesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api V1) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "")
 	r.Body.Close()
 
 	wr.Error(httputil.ErrNotFound, nil)

cmd/dsk/main.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/rundsk/dsk/internal/api"
@@ -62,7 +63,7 @@ func main() {
 	version := flag.Bool("version", false, "print DSK version")
 	noColor := flag.Bool("no-color", false, "disables color output")
 	ffrontend := flag.String("frontend", "", "path to a frontend, to use instead of the built-in")
-	allowOrigin := flag.String("allow-origin", "", "sets a Access-Control-Allow-Origin HTTP header for all API responses")
+	fallowOrigin := flag.String("allow-origin", "", "origins from which browsers can access the HTTP API; for multiple origins, use a comma as a separator, the wildcard * is supported; to allow all use *")
 	flag.Parse()
 
 	if len(flag.Args()) > 1 {
@@ -81,6 +82,7 @@ func main() {
 	}
 	whiteOnBlue := color.New(color.FgWhite, color.BgBlue)
 	green := color.New(color.FgGreen)
+	yellow := color.New(color.FgYellow)
 	red := color.New(color.FgRed)
 
 	if isTerminal {
@@ -114,6 +116,11 @@ func main() {
 	}
 	log.Printf("Detected live path: %s", livePath)
 
+	allowOrigins := strings.Split(*fallowOrigin, ",")
+	if len(allowOrigins) != 0 {
+		log.Print(yellow.Sprintf("Allowing access of the HTTP API from origins: %s", strings.Join(allowOrigins, ", ")))
+	}
+
 	app = plex.NewApp( // assign to global
 		Version,
 		livePath,
@@ -134,15 +141,23 @@ func main() {
 		}
 	}
 
+	mux := http.NewServeMux()
+
 	apis := map[int]httputil.Mountable{
-		1: api.NewV1(app.Sources, app.Version, app.Broker, *allowOrigin),
-		2: api.NewV2(app.Sources, app.Version, app.Broker, *allowOrigin),
+		1: api.NewV1(app.Sources, app.Version, app.Broker, allowOrigins),
+		2: api.NewV2(app.Sources, app.Version, app.Broker, allowOrigins),
 	}
-	for _, a := range apis {
-		a.MountHTTPHandlers()
+	for av, a := range apis {
+		log.Printf("Mounting APIv%d HTTP mux...", av)
+		mux.Handle(
+			fmt.Sprintf("/api/v%d/", av),
+			http.StripPrefix(fmt.Sprintf("/api/v%d", av), a.HTTPMux()),
+		)
 	}
+
 	// Must come last, as it contains a catch all route.
-	app.Frontend.MountHTTPHandlers()
+	log.Print("Mounting frontend HTTP mux...")
+	mux.Handle("/", app.Frontend.HTTPMux())
 
 	addr := fmt.Sprintf("%s:%s", *host, *port)
 	if isTerminal {
@@ -153,7 +168,7 @@ func main() {
 	}
 	log.Printf("Started web interface on %s, in %s", addr, time.Since(start))
 
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(red.Sprintf("Failed to start web interface: %s", err))
 	}
 }

go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/onsi/gomega v1.4.2 // indirect
 	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/rjeczalik/notify v0.9.2
+	github.com/rs/cors v1.7.0
 	github.com/russross/blackfriday v2.0.0+incompatible
 	github.com/shurcooL/httpfs v0.0.0-20171119174359-809beceb2371 // indirect
 	github.com/shurcooL/sanitized_anchor_name v0.0.0-20170918181015-86672fcb3f95 // indirect

go.sum
@@ -110,6 +110,8 @@ github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZb
 github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
 github.com/rjeczalik/notify v0.9.2 h1:MiTWrPj55mNDHEiIX5YUSKefw/+lCQVoAFmD6oQm5w8=
 github.com/rjeczalik/notify v0.9.2/go.mod h1:aErll2f0sUX9PXZnVNyeiObbmTlk5jnMoCa4QEjJeqM=
+github.com/rs/cors v1.7.0 h1:+88SsELBHx5r+hZ8TCkggzSstaWNbDvThkVK8H6f9ik=
+github.com/rs/cors v1.7.0/go.mod h1:gFx+x8UowdsKA9AchylcLynDq+nNFfI8FkUZdN/jGCU=
 github.com/russross/blackfriday v2.0.0+incompatible h1:cBXrhZNUf9C+La9/YpS+UHpUT8YD6Td9ZMSU9APFcsk=
 github.com/russross/blackfriday v2.0.0+incompatible/go.mod h1:JO/DiYxRf+HjHt06OyowR9PTA263kcR/rfWxYHBV53g=
 github.com/sergi/go-diff v1.0.0 h1:Kpca3qRNrduNnOQeazBd0ysaKrUJiIuISHxogkT9RPQ=

internal/api/v2.go
@@ -7,11 +7,11 @@
 package api
 
 import (
-	"log"
 	"net/http"
 	"path/filepath"
 	"time"
 
+	"github.com/rs/cors"
 	"github.com/rundsk/dsk/internal/bus"
 	"github.com/rundsk/dsk/internal/ddt"
 	"github.com/rundsk/dsk/internal/httputil"
@@ -19,21 +19,24 @@ import (
 	"github.com/rundsk/dsk/internal/search"
 )
 
-func NewV2(ss *plex.Sources, appVersion string, b *bus.Broker, allowOrigin string) *V2 {
+func NewV2(ss *plex.Sources, appVersion string, b *bus.Broker, allowOrigins []string) *V2 {
 	return &V2{
-		v1:          NewV1(ss, appVersion, b, allowOrigin),
-		allowOrigin: allowOrigin,
-		sources:     ss,
+		v1:           NewV1(ss, appVersion, b, allowOrigins),
+		allowOrigins: allowOrigins,
+		sources:      ss,
 	}
 }
 
 type V2 struct {
 	v1 *V1
 
-	// The value of the Access-Control-Allow-Origin HTTP header to set, if empty
-	// the header will remain unset. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
-	// for valid values.
-	allowOrigin string
+	// allowOrigins is a list of origins a cross-domain request can be
+	// executed from. If the special * value is present in the list, all
+	// origins will be allowed. An origin may contain a wildcard (*) to
+	// replace 0 or more characters (i.e.: http://*.domain.com). Usage of
+	// wildcards implies a small performance penality. Only one wildcard
+	// can be used per origin. The default value is *.
+	allowOrigins []string
 
 	sources *plex.Sources
 }
@@ -59,24 +62,37 @@ type V2FilterResults struct {
 	Took  int64        `json:"took"` // nanoseconds
 }
 
-func (api V2) MountHTTPHandlers() {
-	log.Print("Mounting APIv2 HTTP handlers...")
+// HTTPMux returns a HTTP mux that can be mounted onto a root mux.
+func (api V2) HTTPMux() http.Handler {
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/api/v2/hello", api.v1.HelloHandler)
-	http.HandleFunc("/api/v2/config", api.v1.ConfigHandler)
-	http.HandleFunc("/api/v2/sources", api.v1.SourcesHandler)
-	http.HandleFunc("/api/v2/tree", api.v1.TreeHandler)
-	http.HandleFunc("/api/v2/tree/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/hello", api.v1.HelloHandler)
+	mux.HandleFunc("/config", api.v1.ConfigHandler)
+	mux.HandleFunc("/sources", api.v1.SourcesHandler)
+	mux.HandleFunc("/tree", api.v1.TreeHandler)
+	mux.HandleFunc("/tree/", func(w http.ResponseWriter, r *http.Request) {
 		if filepath.Ext(r.URL.Path) != "" {
 			api.v1.NodeAssetHandler(w, r)
 		} else {
 			api.v1.NodeHandler(w, r)
 		}
 	})
-	http.HandleFunc("/api/v2/filter", api.FilterHandler)
-	http.HandleFunc("/api/v2/search", api.SearchHandler)
-	http.HandleFunc("/api/v2/messages", api.v1.MessagesHandler)
-	http.HandleFunc("/api/v2", api.v1.NotFoundHandler)
+	mux.HandleFunc("/filter", api.FilterHandler)
+	mux.HandleFunc("/search", api.SearchHandler)
+	mux.HandleFunc("/messages", api.v1.MessagesHandler)
+	mux.HandleFunc("/", api.v1.NotFoundHandler)
+
+	// An empty slice of origins indicates that CORS shoule be
+	// disabled. If we'd pass an empty slice to the CORS middleware
+	// it'd be interpreted to allow all origins. We want to be "secure
+	// by default".
+	if len(api.allowOrigins) == 0 {
+		return mux
+	}
+	return cors.New(cors.Options{
+		AllowedOrigins:   api.allowOrigins,
+		AllowCredentials: true,
+	}).Handler(mux)
 }
 
 func (api V2) NewTreeSearchResults(hs []*search.FullSearchHit, total int, took time.Duration) *V2FullSearchResults {
@@ -109,7 +125,7 @@ func (api V2) NewTreeFilterResults(nodes []*ddt.Node, total int, took time.Durat
 //   /api/v2/search?q={query}
 //   /api/v2/search?q={query}&v={version}
 func (api V2) SearchHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	q := r.URL.Query().Get("q")
@@ -136,7 +152,7 @@ func (api V2) SearchHandler(w http.ResponseWriter, r *http.Request) {
 //   /api/v2/filter?q={query}
 //   /api/v2/filter?q={query}&v={version}
 func (api V2) FilterHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "application/json", api.allowOrigin)
+	wr := httputil.NewResponder(w, r, "application/json")
 	r.Body.Close()
 
 	q := r.URL.Query().Get("q")

internal/frontend/main.go
@@ -36,19 +36,21 @@ type Frontend struct {
 	chroot string
 }
 
-func (f Frontend) MountHTTPHandlers() {
-	log.Print("Mounting frontend HTTP handlers...")
+func (f Frontend) HTTPMux() http.Handler {
+	mux := http.NewServeMux()
 
 	// Handles frontend root document delivery and frontend assets.
 	// The frontend is allowed to use any path except /api. We route
 	// everything else into the front controller (index.html).
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if filepath.Ext(r.URL.Path) != "" {
 			f.AssetHandler(w, r)
 		} else {
 			f.RootHandler(w, r)
 		}
 	})
+
+	return mux
 }
 
 // Serves the frontend's index.html.
@@ -58,7 +60,7 @@ func (f Frontend) MountHTTPHandlers() {
 //   /index.html
 //   /* <catch all>
 func (f *Frontend) RootHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "", "")
+	wr := httputil.NewResponder(w, r, "")
 	r.Body.Close()
 
 	path := "index.html"
@@ -87,7 +89,7 @@ func (f *Frontend) RootHandler(w http.ResponseWriter, r *http.Request) {
 //   /assets/css/base.css
 //   /static/css/main.41064805.css
 func (f *Frontend) AssetHandler(w http.ResponseWriter, r *http.Request) {
-	wr := httputil.NewResponder(w, r, "", "")
+	wr := httputil.NewResponder(w, r, "")
 	r.Body.Close()
 
 	path := r.URL.Path[len("/"):]

internal/httputil/mountable.go
@@ -6,6 +6,9 @@
 
 package httputil
 
+import "net/http"
+
 type Mountable interface {
-	MountHTTPHandlers()
+	// HTTPMux returns a HTTP mux that can be mounted onto a root mux.
+	HTTPMux() http.Handler
 }

internal/httputil/responder.go
@@ -16,8 +16,8 @@ import (
 // HashGetter returns a freshly calculated or cached hash.
 type HashGetter func() (string, error)
 
-func NewResponder(w http.ResponseWriter, r *http.Request, contentType string, allowOrigin string) *Responder {
-	return &Responder{w, r, contentType, allowOrigin}
+func NewResponder(w http.ResponseWriter, r *http.Request, contentType string) *Responder {
+	return &Responder{w, r, contentType}
 }
 
 type Responder struct {
@@ -25,11 +25,6 @@ type Responder struct {
 	r *http.Request
 
 	ContentType string
-
-	// The value of the Access-Control-Allow-Origin HTTP header to set, if empty
-	// the header will remain unset. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
-	// for valid values.
-	allowOrigin string
 }
 
 func (re *Responder) Cached(etag HashGetter) bool {
@@ -55,10 +50,6 @@ func (re *Responder) Cache(etag HashGetter) {
 }
 
 func (re *Responder) OK(data interface{}) {
-	if re.allowOrigin != "" {
-		re.w.Header().Set("Access-Control-Allow-Origin", re.allowOrigin)
-		re.w.Header().Set("Vary", "Origin")
-	}
 	re.w.Header().Set("Content-Type", re.ContentType)
 
 	if re.ContentType != "application/json" {
@@ -80,10 +71,6 @@ func (re *Responder) OK(data interface{}) {
 func (re *Responder) Error(hErr *Error, err error) {
 	if hErr.Code != http.StatusNotFound {
 		log.Printf("Error (masked) while responding to %s: %s", re.r.URL, err)
-	}
-	if re.allowOrigin != "" {
-		re.w.Header().Set("Access-Control-Allow-Origin", re.allowOrigin)
-		re.w.Header().Set("Vary", "Origin")
 	}
 	re.w.Header().Set("Content-Type", re.ContentType)
 	re.w.WriteHeader(hErr.Code)

commit 77174b8f55f600a3bcf864b39a69f649bd207eb0

Fix regression introduced in 7f4fa5b

internal/api/v1.go
@@ -245,7 +245,7 @@ func (api V1) NewNode(n *ddt.Node, s *plex.Source) (*V1Node, error) {
 		return nil, err
 	}
 	for _, v := range nDocs {
-		html, err := v.HTML("/tree", n.URL(), s.Tree.Get, s.Name)
+		html, err := v.HTML("/api/v1/tree", n.URL(), s.Tree.Get, s.Name)
 		if err != nil {
 			return nil, err
 		}