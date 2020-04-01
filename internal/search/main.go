ef5c0dfa7a20f8d8a4b0cdd81ea76dc5c2320c4a
Christoph Labacher <christoph.labacher@gmail.com>
2020-04-01 11:21:27 +0200

    Constant for filter result limit

--- internal/search/main.go
+++ internal/search/main.go
@@ -25,6 +25,8 @@ import (
 	"github.com/rundsk/dsk/internal/ddt"
 )
 
+const FILTER_RESULT_LIMIT = 500
+
 var (
 	// AvailableSearchLangs are languages mapped to their analyzer names.
 	AvailableSearchLangs = map[string]string{
@@ -382,7 +384,7 @@ func (s *Search) FilterSearch(q string) ([]*ddt.Node, int, time.Duration, bool, 
 
 	cq := bleve.NewConjunctionQuery(pqs...)
 	req := bleve.NewSearchRequest(cq)
-	req.Size = 100
+	req.Size = FILTER_RESULT_LIMIT
 
 	res, err := s.narrowIndex.Search(req)
 
