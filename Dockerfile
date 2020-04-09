ce8b257bcacfe35d33286263e2f61ad29b1ed2dc
Marius Wilms <mariuswilms@mailbox.org>
2020-04-09 16:38:14 +0200

    Add copyright holder

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Copyright 2020 Marius Wilms. All rights reserved.
+# Copyright 2020 Marius Wilms, Christoph Labacher. All rights reserved.
 # Copyright 2019 Atelier Disko. All rights reserved.
 #
 # Use of this source code is governed by a BSD-style
--- frontend/src/AssetList/index.js
+++ frontend/src/AssetList/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Marius Wilms. All rights reserved.
+ * Copyright 2020 Marius Wilms, Christoph Labacher. All rights reserved.
  * Copyright 2018 Atelier Disko. All rights reserved.
  *
  * Use of this source code is governed by a BSD-style
